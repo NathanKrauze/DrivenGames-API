f1e2e0b2bc4ebd35b8259f823a578ae44578574a
NathanKrauze <krauzenathan@gmail.com>
2023-09-04 12:03:09 -0300

    feat: add get and post games endpoints

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import gameRouter from './routes/games.routes.js';
 
 const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/test', (req, res) => {
-    res.send('server ok');
-})
+app.use(gameRouter);
 
 const port = process.env.PORT || 5000
-app.listen(port, () => {
+app.listen(port, '127.0.0.1',() => {
 	console.log(`Server running on port ${port}`)
 });
