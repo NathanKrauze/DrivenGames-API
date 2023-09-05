4bf0c523cea3275973aae55b8f77b3f56cfc9352
Nathan Krauze <122039025+NathanKrauze@users.noreply.github.com>
2023-09-05 09:14:02 -0300

    Update app.js, import and use of games.routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import gameRouter from './routes/games.routes.js';
 import router from './routes/index.routes.js';
 
 const app = express();
@@ -7,6 +8,7 @@ app.use(cors())
 app.use(express.json());
 
 app.use(router);
+app.use(gameRouter);
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
