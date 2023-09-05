5ebe893036be02f8c8ad9a2585cdb7229d2d6e7b
PedroGomes2k <ph.gomes2k@gmail.com.br>
2023-09-05 14:47:12 -0300

    feat: get in cart of all games chose

--- src/controllers/pay.controllers.js
+++ src/controllers/pay.controllers.js
@@ -7,7 +7,7 @@ export async function postPay(req, res) {
 
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
-    
+
     if (!token) return response.status(401).send("Token not sended");
 
     const validation = paySchema.validate(req.body, { abortEarly: false })
@@ -18,32 +18,37 @@ export async function postPay(req, res) {
 
     try {
 
-        const session = await db.collection('sessions').findOne({token});
-        console.log(session)
-        if(!session) return res.status(401).send('Unauthorized');
+        const session = await db.collection('sessions').findOne({ token });
 
+        if (!session) return res.status(401).send('Unauthorized');
 
-        if (session) {
+        const user = await db.collection("users").findOne({ _id: session.userID })
+
+        if (user) {
+
 
             delete user.password
-            
+
 
             if (!cardNumber) return res.status(422).send("The card number have a problem")
             if (!securityNumber) return res.status(422).send("The security number it is wrong")
             if (!expirationDate) return res.status(422).send("The expirate date have a problema")
 
+           
             const userCard = {
+                userID: user._id,
                 cardName,
                 cardNumber,
                 securityNumber,
                 expirationDate
             }
+        
 
             await db.collection("userPay").insertOne(userCard)
 
             return res.status(200).send("OK")
         }
-        
+
 
     } catch (err) {
         res.status(500).send(err.message);
@@ -56,6 +61,15 @@ export async function getPay(req, res) {
     if (!token) return response.status(401).send("Token not sended");
 
     try {
+
+        const session = await db.collection('sessions').findOne({ token });
+
+        if (!session) return res.status(401).send('Unauthorized');
+
+        const user = await db.collection("users").findOne({ _id: session.userID })
+
+        console.log(user)
+        await res.status(200).send("OK")
 
     } catch (err) {
         res.status(500).send(err.message);
--- src/schemas/pay.schemas.js
+++ src/schemas/pay.schemas.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const paySchema = Joi.object({
     cardName:Joi.string().required(),
-    cardNumber:Joi.number().required(),
+    cardNumber:Joi.string().min(16).max(19).required(),
     securityNumber:Joi.number().required(),
     expirationDate:Joi.string().required()
 })
