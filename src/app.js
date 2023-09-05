6925905fbff709290fcb187f8bb8dee54ddd16e8
NathanKrauze <krauzenathan@gmail.com>
2023-09-05 09:24:21 -0300

    refactor: corrects routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import gameRouter from './routes/games.routes.js';
 import router from './routes/index.routes.js';
 
 const app = express();
@@ -8,7 +7,6 @@ app.use(cors())
 app.use(express.json());
 
 app.use(router);
-app.use(gameRouter);
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
